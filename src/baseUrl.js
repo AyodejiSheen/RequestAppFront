edd1239c26bc7d10aaecbe017594658b46118c27
AyodejiSheen <aoluwasegun009@gmail.com>
2022-07-08 09:07:19 +0100

    jld

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,6 +1,6 @@
 export default {
     //change base url to local host when working locally or to heroku api url when deployed
-    // baseUrl : 'https://requestappserver.herokuapp.com'
-    baseUrl : 'http://localhost:5000'
+    baseUrl : 'https://requestappserver.herokuapp.com'
+    // baseUrl : 'http://localhost:5000'
 }
 
